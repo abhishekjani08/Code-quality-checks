9144199c71b6153f6688339712decf09654eae0c
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2023-09-24 21:53:08 +0530

    linted the file

--- testFile.js
+++ testFile.js
@@ -1,33 +1,30 @@
-let sum = (a, b) => {
+const sum = (a, b) => {
+  return a+b;
+};
+
+// Missing semicolon and extra spacing
+const multiply = (a, b) => { return a * b }
+
+// Unused variable 'c'
+const unusedVar = (a, b, c) => {
   return a + b;
 };
-console.log(sum(3, 4));
 
-const multiply = (a, b) =>{
-  return a * b;
-};
-console.log(multiply(3, 4));
-
-const unusedVar = (a, b, c) =>{
-  return a + b + c;
-};
-console.log(unusedVar(3, 4, 5));
-
-if (sum(1, 2) === 3) {
+// '==' should be '==='
+if (sum(1, 2) == 3) {
   console.log('This is not type-safe.');
 }
 
 // Trailing whitespace at the end of the line
-const divide = (a, b) => {
+const divide = (a, b) => {  
   if (b === 0) {
     return 'Cannot divide by zero.';
   }
   return a / b;
 };
-console.log(divide(3, 4));
 
-let outdatedVar = 'I am outdated variable declaration';
-console.log(outdatedVar);
+// 'var' instead of 'let' or 'const'
+var outdatedVar = "I'm an outdated variable declaration";
 
-let undeclaredVar = 'I am undeclared';
-console.log(undeclaredVar);
+// Global variable without declaration
+undeclaredVar = 'I am undeclared';
